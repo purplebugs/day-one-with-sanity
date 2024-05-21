4be5e7d93bced340bd422d494a81968c08ef0bfc
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-21 13:39:46 +0200

    feat: Structure customisation

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,8 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {structure} from './structure'
+import {defaultDocumentNode} from './structure/defaultDocumentNode'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,7 @@ export default defineConfig({
   projectId: 'phuq8fkg',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure, defaultDocumentNode}), structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
